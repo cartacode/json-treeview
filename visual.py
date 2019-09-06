2b9e08781a2a87b36808000faf6557e9125cd3b3
it-avenger <simbawow8@gmail.com>
2019-09-06 05:19:47 -0400

    add treeview

--- visual.py
+++ visual.py
@@ -5,12 +5,18 @@ import pandas as pd
 import json
 import sys
 import os
+import copy
 from graphviz import Digraph
 import tkinter as Tkinter
 import tkinter.ttk as ttk
 from treelib import Node, Tree
 import pdb
 
+
+class Node:
+    def __init__(self, name, obj=None):
+        self.name = name
+        self.obj = obj
 
 class TreeView(Tkinter.Frame):
     '''
@@ -22,7 +28,10 @@ class TreeView(Tkinter.Frame):
         '''
         Tkinter.Frame.__init__(self, parent)
         self.parent=parent
-        self.initialize_user_interface()
+        self.item_counts = 0
+        self.headers = []
+        self.idx = 1 # index number of node
+        self.node_hash = {}
 
     """
         creates a json view from excel file
@@ -36,6 +45,8 @@ class TreeView(Tkinter.Frame):
             sheet_content = []
 
             row_counts = xl_file[sheet_name].index.stop
+            self.headers = list(xl_file[sheet_name])
+
             for idx in range(0, row_counts):
                 row_data = {}
                 for row_name in xl_file[sheet_name][:1]:
@@ -55,44 +66,55 @@ class TreeView(Tkinter.Frame):
     # Create your views here.
     def get_treeview(self, filename):
         excel_data = self.get_data_from_excel(filename)
-
-        # make a tree
-        result_hash = {} # hierarchy structure response
-        children_hash = {}
-        records_hash = {}
+        self.initialize_user_interface()
 
         for item in excel_data:
             iteritems = item['sheet_content']
-            for iteritem in iteritems:
-                records_hash[str(iteritem["id"])] = iteritem
-                if iteritem["parent"] == None:
-                    result_hash[str(iteritem["id"])] = iteritem
-                else:
-                    if len(children_hash) > 0 and str(iteritem["parent"]) in children_hash:
-                        children_hash[str(iteritem["parent"])].append(iteritem)
-                    else:
-                        children_hash[str(iteritem["parent"])] = [iteritem]
+            items_tree = self.treeify(iteritems)
+        
+        self.insert_data("", 0, 0, "treeview", ())
+        for item in self.node_hash.values():
+            set_value_obj = ()
+            for key_of_obj in item['info']:
+                set_value_obj = set_value_obj + (item['info'][key_of_obj],)
+            print("@@@@@@@@@@@@@@@@@@@@: ", item['parent'], item['idx'], "****", item)
+            self.insert_data(item['parent'], item['node_type'], item['idx'], item['text'], set_value_obj)
 
-        while len(children_hash.keys()) > 0:
-            for key in  list(children_hash):
-                parent = records_hash[key]["parent"]
-                if parent is None:
-                    if 'children' in result_hash[key]:
-                        result_hash[key]["children"].append(children_hash[key])
-                    else:
-                        result_hash[key]["children"] = children_hash[key]
+    def treeify(self, elements, idAttr='id', parentAttr='parent', childrenAttr='children'):
+        treeList = []
+        lookup = {}
 
-                else:
-                    parent = str(records_hash[key]["parent"])
-                    if parent in children_hash:
-                        children_hash[parent].append(children_hash[key])
-                    else:
-                        children_hash[parent] = children_hash[key]
+        for element in elements:
+            
+            lookup[element[idAttr]] = element
+            element[childrenAttr] = []
 
-                del children_hash[key]
+        for element in elements:
+            parent_idx = 0
+            node_type = "end"
+            if element[parentAttr] in self.node_hash:
+                if 'idx' in self.node_hash[element[parentAttr]]:
+                    parent_idx = self.node_hash[element[parentAttr]]['idx']
+                    node_type = self.idx
 
-        k = list(result_hash)[0]
-        print(result_hash[k])
+            info = copy.deepcopy(element)
+            del info['children']
+            self.node_hash[element[idAttr]] = {
+                'idx': self.idx,
+                'parent': parent_idx,
+                'node_type': node_type,
+                'text': element['keyword singular de'],
+                'info': info
+            }
+
+            self.idx = self.idx + 1
+
+            if element[parentAttr] is not None:
+                lookup[element[parentAttr]][childrenAttr].append(element)
+            else:
+                treeList.append(element)
+
+        return treeList
 
     def initialize_user_interface(self):
         """Draw a user interface allowing the user to type
@@ -104,64 +126,38 @@ class TreeView(Tkinter.Frame):
         self.parent.config(background="lavender")
 
         # Set the treeview
-        self.tree = ttk.Treeview(self.parent,
-                                 columns=('Dose', 'Modification date'))
-        self.tree.heading('#0', text='Item')
-        self.tree.heading('#1', text='Dose')
-        self.tree.heading('#2', text='Modification Date')
-        self.tree.column('#1', stretch=Tkinter.YES)
-        self.tree.column('#2', stretch=Tkinter.YES)
-        self.tree.column('#0', stretch=Tkinter.YES)
-        self.tree.grid(row=4, columnspan=4, sticky='nsew')
+        self.tree = ttk.Treeview(self.parent)
+        print('initialize view: ', self.headers)
+
+        self.tree["columns"]=(self.headers)
+        for i, header in enumerate(self.headers):
+            self.tree.column('#{}'.format(str(i)), stretch=Tkinter.YES)
+
+        for i, header in enumerate(self.headers, start=1):
+            print('#{}'.format(str(i)), header)
+            self.tree.heading('#{}'.format(str(i)), text=header)
+
+        self.tree.grid(row=len(self.headers)+1, columnspan=4, sticky='nsew')
         self.treeview = self.tree
         # Initialize the counter
-        self.i = 0
+        self.item_counts = 0
 
-    def insert_data(self):
+    def insert_data(self, target="", node_type="end", idx=1, text="item", set_value_obj=None):
         """
         Insertion method.
         """
-        self.treeview.insert('', 'end', text="Item_"+str(self.i),
-                             values=(self.dose_entry.get() + " mg",
-                                     self.modified_entry.get()))
+        self.treeview.insert(target, node_type, idx, text=text,
+                             values=set_value_obj)
         # Increment counter
-        self.i = self.i + 1
+        self.item_counts = self.item_counts + 1
 
 if __name__ == "__main__":
     folder_path = os.getcwd() + '/inputs' # path where the xlsx files are
     file_name = os.path.join(folder_path, 'input.xlsx')
 
-    json_tree = JsonView()
-    # json_tree.get_treeview(file_name)
-
     root = Tkinter.Tk()
-    root.title("JSON editor")
-    d = TreeView(root)
+    root.title("JSON TreeView")
+    tree = TreeView(root)
+    tree.get_treeview(file_name)
     root.mainloop()
-
-    # tree=ttk.Treeview(root)
-
-    # tree["columns"]=("one","two","three")
-    # tree.column("#0", width=270, minwidth=270, stretch=tk.NO)
-    # tree.column("one", width=150, minwidth=150, stretch=tk.NO)
-    # tree.column("two", width=400, minwidth=200)
-    # tree.column("three", width=80, minwidth=50, stretch=tk.NO)
-
-    # tree.heading("#0",text="Name",anchor=tk.W)
-    # tree.heading("one", text="Date modified",anchor=tk.W)
-    # tree.heading("two", text="Type",anchor=tk.W)
-    # tree.heading("three", text="Size",anchor=tk.W)
-
-    # # Level 1
-    # folder1=tree.insert("", 1, 1, text="Folder 1", values=("23-Jun-17 11:05","File folder",""))
-    # folder2=tree.insert(folder1, 1, 5, text="Folder 1", values=("23-Jun-17 11:05","File folder",""))
-    # folder2=tree.insert(folder1, 1, 4, text="Folder 1", values=("23-Jun-17 11:05","File folder",""))
-    # # tree.insert("", 2, "", text="text_file.txt", values=("23-Jun-17 11:25","TXT file","1 KB"))
-    # # Level 2
-    # tree.insert(folder2, "end", 2, text="photo1.png", values=("23-Jun-17 11:28","PNG file","2.6 KB"))
-    # tree.insert(folder2, "end", 3, text="photo2.png", values=("23-Jun-17 11:29","PNG file","3.2 KB"))
-    # tree.insert(folder2, "end", 4, text="photo3.png", values=("23-Jun-17 11:30","PNG file","3.1 KB"))
-
-    # tree.pack(side=tk.TOP,fill=tk.X)
-    # root.mainloop()
 
